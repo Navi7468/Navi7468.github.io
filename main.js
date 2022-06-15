c984503d9ce19ca4465dfc3fdf096d0c7d169070
Navi <96448085+Navi7468@users.noreply.github.com>
2022-06-15 15:47:59 -0700

    TSS (0.0.2)

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
 window.onscroll = function() {myFunction()};
 
 var navbar = document.getElementById("navbar");
+var a = document.getElementsByTagName('a');
 var sticky = navbar.offsetTop;
+
 
 function myFunction() {
     if (window.pageYOffset >= sticky) {
         navbar.classList.add("sticky")
+        for (var i = 0; i < a.length; i++) {
+            a[i].classList.add("sticky-nav-link");
+            a[i].classList.remove("navbar-link");
+        }    
     } else {
         navbar.classList.remove("sticky");
+        for (var i = 0; i < a.length; i++) {
+            a[i].classList.remove("sticky-nav-link");
+            a[i].classList.add("navbar-link");
+        }
     }
 }

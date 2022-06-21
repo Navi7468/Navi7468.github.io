1ee7eeaa91a900297da9c9578c4ffc25f378bc9f
Navi <96448085+Navi7468@users.noreply.github.com>
2022-06-21 13:12:39 -0700

    TSS (v0.0.6)

--- main.js
+++ main.js
@@ -1,22 +1,31 @@
-window.onscroll = function() {myFunction()};
-
 var navbar = document.getElementById("navbar");
+var navSpace = document.getElementById("nav-space");
 var a = document.getElementsByTagName('#navbar-link');
-var sticky = navbar.offsetTop;
+let sticky;
+
+window.onresize = () => {
+    sticky = navbar.offsetTop
+}
+
+sticky = navbar.offsetTop
+
+window.onscroll = function() {myFunction()};
 
 console.log(navbar);
 console.log(sticky);
 
 
 function myFunction() {
-    console.log(window.pageYOffset);
+    // console.log(window.pageYOffset);
     if (window.pageYOffset > sticky) {
+        navSpace.style.display = "block";
         navbar.classList.add("sticky")
         for (var i = 0; i < a.length; i++) {
             a[i].classList.add("sticky-nav-link");
             a[i].classList.remove("navbar-link");
         }    
     } else {
+        navSpace.style.display = "none";
         navbar.classList.remove("sticky");
         for (var i = 0; i < a.length; i++) {
             a[i].classList.remove("sticky-nav-link");
